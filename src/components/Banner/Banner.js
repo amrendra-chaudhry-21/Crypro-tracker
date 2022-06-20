37af65d820abaa2d221bb2339c3214297e31939f
amrendrait43@gmail.com <Nh2conh2@aa432>
2022-06-20 12:36:17 +0530

    Tracker

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -41,7 +41,7 @@ function Banner() {
               fontFamily: "Montserrat",
             }}
           >
-            Crypto Tracker
+            Crypto Hunter
           </Typography>
           <Typography
             variant="subtitle2"
